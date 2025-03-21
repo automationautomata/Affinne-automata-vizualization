c32bf664e6594c8d5303e22b6a30af48aeb3f919
automationautomata <32201655+automationautomata@users.noreply.github.com>
2025-03-22 02:49:31 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from PyQt6.QtWidgets import QApplication
 from widgets.mainwindow import MainWindow
 from sys import exit, argv
 
-# Только для доступа к аргументам командной строки
-# Приложению нужен один (и только один) экземпляр QApplication.
-# Передаём sys.argv, чтобы разрешить 
-# аргументы командной строки для приложения.
-# Если не будете использовать аргументы командной строки, 
-# QApplication([]) тоже работает
 app = QApplication(argv)
 
 # Создаём виджет Qt — окно.
